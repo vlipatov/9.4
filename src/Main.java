2aedf8ad85eda49b72c802c9053c55f33e685843
vlipatov <vadegg0607@gmail.com>
2020-02-04 15:41:50 +0300

    working one

--- src/Main.java
+++ src/Main.java
@@ -2,42 +2,34 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.MalformedURLException;
+import java.io.*;
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         Document doc = Jsoup.connect("http://lenta.ru/").get();
         Elements elements = doc.select("img");
-
+        String path = "data/images";
         elements.forEach(e -> {
             try {
-                downloader(e.absUrl("src"), "/data/pics");
+                downloader(e.absUrl("src"), path);
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
         });
     }
 
-
     public static void downloader(String url, String path) throws IOException {
-        URL realURL = new URL(url);
-        BufferedImage img = ImageIO.read(realURL);
-        Path destDir = Paths.get(path);
-        if (!destDir.toFile().isDirectory()) { // если по пути назначения нет папки, то создаем новую
-            Files.createDirectory(destDir);
+        int index = url.lastIndexOf("/");
+        /** проверка чтобы не включались две ссылки - https://mc.yandex.ru/watch/27714477 ,
+         *  https://counter.rambler.ru/top100.cnt?pid=80674 */
+        if (url.matches(".*(\\.[a-z]{3})$")) {
+            try (InputStream in = new URL(url).openStream()) {
+                Files.copy(in, Paths.get(path + url.substring(index)), StandardCopyOption.REPLACE_EXISTING);
+            }
         }
-        ImageIO.write(img, "jpg", (OutputStream) destDir);
     }
 }
 
